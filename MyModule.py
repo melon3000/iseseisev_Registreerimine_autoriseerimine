d86990dbbcf31640288d71954bdf32e738ccc6c6
Milan <milan.petrovski8@gmail.com>
2025-03-24 15:53:24 +0200

    init

--- MyModule.py
+++ MyModule.py
@@ -1,18 +1,68 @@
+from ast import Str
+import os
+import random
+from os import system
+from time import sleep
+
 YELLOW = "\033[93m"
 GREEN = "\033[92m"
 RED = '\033[31m'
-RESET = "\033[0m"
+G = '\033[90m'
+R = "\033[0m"
 
-def checkpassword(password):
+#----------------------------------------------------------------------------------
+def title():
+    """Aken pealkirja animatsioon.
+    Kuvab "github.com/melon3000" tähemärkide kaupa akna pealkirjana.
+    """
+    system('cls')
+    text = ''
+    for char in "github.com/melon3000":
+        text += char
+        system(f'title {text}')
+        sleep(0.01)
+    system('title github.com/melon3000')
+
+#----------------------------------------------------------------------------------
+def get_user(user_login_status: bool, login: str = '') -> str:
+    """Tagastab aktiivse kasutaja nime."""
+    if user_login_status == 0:
+        return os.getenv("USERNAME")
+    elif user_login_status == 1:
+        return login
+
+#----------------------------------------------------------------------------------
+def menu(current_user: str) -> int:
+    """Kuvab valikumenüü ja tagastab kasutaja valiku."""
+    while True:
+        try:
+            system('cls')
+            print(f"""{G}Logitud kui: {GREEN}{current_user}{G}\n
+┌[{GREEN}1{G}] - Registreerimine
+├[{GREEN}2{G}] - Autoriseerimine
+├[{GREEN}3{G}] - Nime või parooli muutmine
+├[{GREEN}4{G}] - Unustanud parooli taastamine
+└[{GREEN}5{G}] - Lõpetamine
+""")
+            func_choice = int(input("> "))
+            if 1 <= func_choice <= 5:
+                return func_choice
+            else:
+                print(f"{RED}Sisetage ainult numbrid 1-5!{R}")
+                sleep(1)
+        except ValueError:
+            print(f"{RED}Sisetage ainult numbrid 1-5!{R}")
+            sleep(1)
+
+#----------------------------------------------------------------------------------
+def checkpassword(password: str) -> bool:
+    """Kontrollib, kas parool vastab turvanõuetele."""
     symbols1 = list(".,:;!_*-+()/#¤%&")
     symbols2 = list("0123456789")
     symbols3 = list('qwertyuiopasdfghjklzxcvbnm')
     symbols4 = list('QWERTYUIOPASDFGHJKLZXCVBNM')
 
-    has_symbols = False
-    has_digits = False
-    has_lowercase = False
-    has_uppercase = False
+    has_symbols = has_digits = has_lowercase = has_uppercase = False
 
     for char in password:
         if char in symbols1:
@@ -25,60 +75,126 @@ def checkpassword(password):
             has_uppercase = True
         
         if has_symbols and has_digits and has_lowercase and has_uppercase:
-            print(f'{GREEN}Parool vastab normile{RESET}.')
+            print(f'{GREEN}Parool vastab normile{R}.')
+            sleep(3)
             return True
-    print(f'{RED}Parool ei vastab normile{RESET}.')
+    print(f'{RED}Parool ei vasta normile!{R}')
+    sleep(3)
     return False
 
-
-def autopassword():
-    import random
-    str0=".,:;!_*-+()/#¤%&"
+#----------------------------------------------------------------------------------
+def generatePassword() -> str:
+    """Genereerib juhusliku turvalise parooli."""
+    str0 = ".,:;!_*-+()/#¤%&"
     str1 = '0123456789'
     str2 = 'qwertyuiopasdfghjklzxcvbnm'
     str3 = str2.upper()
-    str4 = str0+str1+str2+str3
+    str4 = str0 + str1 + str2 + str3
     ls = list(str4)
     random.shuffle(ls)
-    psword = ''.join([random.choice(ls) for x in range(12)])
-    return psword
+    return ''.join([random.choice(ls) for i in range(12)])
 
-
-def register(users): #edastame sõnastiku *users*, et funktsioon vottis olevad andmeid (ehk {username:password})
-    """Funktsioon loob uut kasutaja, kui tema juba ei ole loodud ja nimi pole kasutatud.
-    :param users: {nimi:parool} sõnastikust,
-    """
-    username = input("Sisesta nimi: ")
+#----------------------------------------------------------------------------------
+def register(users: dict):
+    """Registreerib uue kasutaja."""
+    system('cls')
+    username = input(f"{G}Sisesta nimi: {R}")
     
-    if username.lower() in (name.lower() for name in users): #kontrollime, kas user on juba registreerinud voi ei
-
-        print("Viga! Nimi on juba kasutusel.")
+    if username.lower() in (name.lower() for name in users):
+        print(f"{RED}Viga! Nimi on juba kasutusel.{R}")
+        sleep(3)
         return 
     
-    password_method_choice = int(input("1 - Autogenereerimine, 2 - Manuaalne: "))
-    
+    password_method_choice = int(input(f"{GREEN}1 {G}- Autogenereerimine, {GREEN}2 {G}- Manuaalne: {R}{GREEN}"))
+    print(f'{R}')    
+
     if password_method_choice == 1:
-        users[username] = autopassword()
-        print(f"Kasutaja {username} on registreeritud!")
-        print("Teie parool:", users[username])
-        return
+        users[username] = generatePassword()
+        print(f"{G}Kasutaja {GREEN}{username}{G} on registreeritud!")
+        print(f"{R}Teie parool:{GREEN}{users[username]}{G}")
+        system('echo. & echo Vajuta mõni nupp... & pause >nul')
         
     elif password_method_choice == 2:
         password = input("Sisesta parool: ")
-        if checkpassword(password) == True:
-            users[username] = password #määrame uuele kasutajale parooli ja kirjutame selle sõnastikku
+        if checkpassword(password):
+            users[username] = password
+            print(f"{G}Kasutaja {GREEN}{username}{G} on registreeritud!")
+    sleep(3)
 
-            print(f"Kasutaja {username} on registreeritud!")
+#----------------------------------------------------------------------------------
+def auth(username: str, password: str, users: dict):
+    """Kontrollib, kas kasutaja ja parool on õiged."""
+    system('cls')
 
-def auth(username:str, password:str)->any:
-    """
-    """
+    if username.lower() in (name.lower() for name in users):
+        if users[username.lower()] == password:
+            print(f"Õnnitlused, {GREEN}{username}{R}! Olete edukalt sisse logitud.")
+            sleep(3)
+            return username, 1
+        else:
+            print(f"{RED}Vale parool!{R}")
+            sleep(3)
+    else:
+        print(f"{RED}Pole kasutajat seda nimega.{R}")
+        sleep(3)
+    return None, 0
 
-def userData_Rhange(username:str, password:str)->any:
+#----------------------------------------------------------------------------------
+def change_credentials(users: dict, username: str, login: str, user_login_status: bool):
+    """Võimaldab kasutajal muuta oma nime või parooli, kui ta on sisse logitud ja tema login vastab sisestatud kasutajale.
+    
+    :param dict users: kasutajate andmebaas
+    :param str username: aktiivne kasutajanimi
+    :param str login: sisse logitud kasutaja nimi
+    :param bool user_login_status: kas kasutaja on sisse logitud (1) või mitte (0)
     """
-    """
+    system('cls')
 
-def userData_Restore(username:str, password:str)->any:
-    """
-    """
+    # Kontrollime, kas kasutaja on sisse logitud ja kas login vastab username-le
+    if user_login_status == 0 or username != login:
+        print(f"{RED}Viga! Peate olema sisse logitud ja kasutama oma andmeid.{R}")
+        sleep(3)
+        return
+    
+    print(f"""{G}Mida soovite muuta?
+┌[{GREEN}1{G}] - Muuda kasutajanime
+└[{GREEN}2{G}] - Muuda parooli
+""")
+    choice = input("> ")
+    
+    if choice == "1":
+        new_username = input(f"{G}Sisesta uus kasutajanimi: {R}")
+        if new_username in users:
+            print(f"{RED}Viga! See nimi on juba kasutusel.{R}")
+            sleep(3)
+            return
+        users[new_username] = users.pop(username)
+        print(f"{GREEN}Kasutajanimi muudetud! Uus nimi: {new_username}{R}")
+    
+    elif choice == "2":
+        new_password = input(f"{G}Sisesta uus parool: {R}")
+        if checkpassword(new_password):
+            users[username] = new_password
+            print(f"{GREEN}Parool edukalt muudetud!{R}")
+    
+    else:
+        print(f"{RED}Vale valik!{R}")
+    
+    sleep(3)
 
+#----------------------------------------------------------------------------------
+def restorePassword(user_login_status: bool, login:str, username: str, users: dict):
+    """Laseb kasutajal parooli lähtestada."""
+
+    if user_login_status == 0 or username != login:
+        print(f"{RED}Viga! Peate olema sisse logitud ja kasutama oma andmeid.{R}")
+        sleep(2)
+        return
+
+    if username in users and username == login:
+        new_password = generatePassword()
+        users[username] = new_password
+        print(f"{GREEN}Uus parool kasutajale {username}: {new_password}{R}")
+    else:
+        print(f"{RED}Kasutajat ei leitud!{R}")
+    sleep(2)
--- iseseisev_Registreerimine_autoriseerimine.py
+++ iseseisev_Registreerimine_autoriseerimine.py
@@ -1,9 +1,8 @@
 from MyModule import *
-from time import sleep
-from os import system
-system('title @melon3000')
+title()
 
-current_user = ""
+user_login_status = 0
+login = ''
 
 users = {
     "user1": "password123",
@@ -11,4 +10,30 @@ users = {
     "john_doe": "qwerty123"
 }
 
-register(users)
+while True:
+    current_user = get_user(user_login_status, login)
+    func_choice = menu(current_user)
+
+    if func_choice == 1:
+        print("Registreerimine valitud.")
+        sleep(0.5)
+        register(users)
+
+    elif func_choice == 2:
+        print("Autoriseerimine valitud.")
+        sleep(0.5)
+        username = input(f"{G}Sisesta nimi: {R}")
+        password = input(f"{G}Sisesta parool: {R}")
+        login, user_login_status = auth(username, password, users)
+
+    elif func_choice == 3:
+        print("Nime või parooli muutmine.")
+        sleep(0.5)
+        username = input(f"{G}Sisesta nimi: {R}")
+        change_credentials(users, username, login, user_login_status)
+
+    elif func_choice == 4:
+        print("Unustanud parooli taastamine.")
+        sleep(0.5)
+        username = input(f"{G}Sisesta nimi: {R}")
+        restorePassword(user_login_status, login, username, users)
